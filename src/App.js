14b8e1cdbc11b48c9f09b63deddc6c807fa6bc30
yuichi1978 <98740868+yuichi1978@users.noreply.github.com>
2022-03-25 08:18:01 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Router } from "./router/Router";
+import "./styles.css";
+
+const user = {
+  name: "じゃけぇ",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
+
+export function App() {
+  return <Router />;
+}
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;
